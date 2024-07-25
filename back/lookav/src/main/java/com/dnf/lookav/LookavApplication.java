58e324d8831deeaf34c4bb9aabea0024c03ad71f
Jaeseong <easeon78@gmail.com>
2024-07-25 17:24:10 +0900

    init project

---
+++ back/lookav/src/main/java/com/dnf/lookav/LookavApplication.java
@@ -0,0 +1,13 @@
+package com.dnf.lookav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookavApplication.class, args);
+	}
+
+}
---
+++ back/lookav/src/test/java/com/dnf/lookav/LookavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnf.lookav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
